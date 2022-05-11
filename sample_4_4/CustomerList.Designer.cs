aa114d45b2be8b64626b8f89c560c3f6fdadc817
nirzaf <mfmfazrin1986@gmail.com>
2022-05-11 20:04:40 +0530

    Add project files.

---
+++ sample_4_4/CustomerList.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace sample_4_4 {
+  partial class CustomerList {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing) {
+      if (disposing && (components != null)) {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent() {
+      this.components = new System.ComponentModel.Container();
+      this.customerCollection = new DevExpress.Xpo.XPCollection(this.components);
+      this.unitOfWork = new DevExpress.Xpo.UnitOfWork(this.components);
+      this.saveChangesItem = new DevExpress.XtraBars.BarButtonItem();
+      this.discardChangesItem = new DevExpress.XtraBars.BarButtonItem();
+      this.printExportItem = new DevExpress.XtraBars.BarButtonItem();
+      this.ribbonControl1 = new DevExpress.XtraBars.Ribbon.RibbonControl();
+      this.ribbonPageCategory1 = new DevExpress.XtraBars.Ribbon.RibbonPageCategory();
+      this.ribbonPage1 = new DevExpress.XtraBars.Ribbon.RibbonPage();
+      this.ribbonPageGroup1 = new DevExpress.XtraBars.Ribbon.RibbonPageGroup();
+      this.ribbonPageGroup2 = new DevExpress.XtraBars.Ribbon.RibbonPageGroup();
+      this.gridControl1 = new DevExpress.XtraGrid.GridControl();
+      this.gridView1 = new DevExpress.XtraGrid.Views.Grid.GridView();
+      this.colOid = new DevExpress.XtraGrid.Columns.GridColumn();
+      this.colName = new DevExpress.XtraGrid.Columns.GridColumn();
+      this.gridColumn1 = new DevExpress.XtraGrid.Columns.GridColumn();
+      this.gridColumn2 = new DevExpress.XtraGrid.Columns.GridColumn();
+      this.gridColumn3 = new DevExpress.XtraGrid.Columns.GridColumn();
+      this.gridColumn4 = new DevExpress.XtraGrid.Columns.GridColumn();
+      ((System.ComponentModel.ISupportInitialize)(this.customerCollection)).BeginInit();
+      ((System.ComponentModel.ISupportInitialize)(this.unitOfWork)).BeginInit();
+      ((System.ComponentModel.ISupportInitialize)(this.ribbonControl1)).BeginInit();
+      ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).BeginInit();
+      ((System.ComponentModel.ISupportInitialize)(this.gridView1)).BeginInit();
+      this.SuspendLayout();
+      // 
+      // customerCollection
+      // 
+      this.customerCollection.ObjectType = typeof(EventsDB.Customer);
+      this.customerCollection.Session = this.unitOfWork;
+      // 
+      // saveChangesItem
+      // 
+      this.saveChangesItem.Caption = "Save Changes";
+      this.saveChangesItem.Id = 1;
+      this.saveChangesItem.ImageOptions.SvgImage = global::sample_4_4.Properties.Resources.savechanges;
+      this.saveChangesItem.Name = "saveChangesItem";
+      // 
+      // discardChangesItem
+      // 
+      this.discardChangesItem.Caption = "Discard Changes";
+      this.discardChangesItem.Id = 2;
+      this.discardChangesItem.ImageOptions.SvgImage = global::sample_4_4.Properties.Resources.discardchanges;
+      this.discardChangesItem.Name = "discardChangesItem";
+      // 
+      // printExportItem
+      // 
+      this.printExportItem.Caption = "Print && Export";
+      this.printExportItem.Id = 3;
+      this.printExportItem.ImageOptions.SvgImage = global::sample_4_4.Properties.Resources.printexport;
+      this.printExportItem.Name = "printExportItem";
+      // 
+      // ribbonControl1
+      // 
+      this.ribbonControl1.ExpandCollapseItem.Id = 0;
+      this.ribbonControl1.Items.AddRange(new DevExpress.XtraBars.BarItem[] {
+            this.ribbonControl1.ExpandCollapseItem,
+            this.ribbonControl1.SearchEditItem,
+            this.saveChangesItem,
+            this.discardChangesItem,
+            this.printExportItem});
+      this.ribbonControl1.Location = new System.Drawing.Point(0, 0);
+      this.ribbonControl1.MaxItemId = 1;
+      this.ribbonControl1.Name = "ribbonControl1";
+      this.ribbonControl1.PageCategories.AddRange(new DevExpress.XtraBars.Ribbon.RibbonPageCategory[] {
+            this.ribbonPageCategory1});
+      this.ribbonControl1.Size = new System.Drawing.Size(816, 150);
+      // 
+      // ribbonPageCategory1
+      // 
+      this.ribbonPageCategory1.Appearance.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+      this.ribbonPageCategory1.Appearance.Options.UseBackColor = true;
+      this.ribbonPageCategory1.Name = "ribbonPageCategory1";
+      this.ribbonPageCategory1.Pages.AddRange(new DevExpress.XtraBars.Ribbon.RibbonPage[] {
+            this.ribbonPage1});
+      this.ribbonPageCategory1.Text = "Customers";
+      // 
+      // ribbonPage1
+      // 
+      this.ribbonPage1.Groups.AddRange(new DevExpress.XtraBars.Ribbon.RibbonPageGroup[] {
+            this.ribbonPageGroup1,
+            this.ribbonPageGroup2});
+      this.ribbonPage1.Name = "ribbonPage1";
+      this.ribbonPage1.Text = "Data Operations";
+      // 
+      // ribbonPageGroup1
+      // 
+      this.ribbonPageGroup1.ItemLinks.Add(this.saveChangesItem);
+      this.ribbonPageGroup1.ItemLinks.Add(this.discardChangesItem);
+      this.ribbonPageGroup1.Name = "ribbonPageGroup1";
+      this.ribbonPageGroup1.Text = "Persistence";
+      // 
+      // ribbonPageGroup2
+      // 
+      this.ribbonPageGroup2.ItemLinks.Add(this.printExportItem);
+      this.ribbonPageGroup2.Name = "ribbonPageGroup2";
+      this.ribbonPageGroup2.Text = "Tools";
+      // 
+      // gridControl1
+      // 
+      this.gridControl1.DataSource = this.customerCollection;
+      this.gridControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+      this.gridControl1.Location = new System.Drawing.Point(0, 150);
+      this.gridControl1.MainView = this.gridView1;
+      this.gridControl1.MenuManager = this.ribbonControl1;
+      this.gridControl1.Name = "gridControl1";
+      this.gridControl1.Size = new System.Drawing.Size(816, 422);
+      this.gridControl1.TabIndex = 1;
+      this.gridControl1.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.gridView1});
+      // 
+      // gridView1
+      // 
+      this.gridView1.Columns.AddRange(new DevExpress.XtraGrid.Columns.GridColumn[] {
+            this.colOid,
+            this.colName,
+            this.gridColumn1,
+            this.gridColumn2,
+            this.gridColumn3,
+            this.gridColumn4});
+      this.gridView1.GridControl = this.gridControl1;
+      this.gridView1.Name = "gridView1";
+      // 
+      // colOid
+      // 
+      this.colOid.FieldName = "Oid";
+      this.colOid.Name = "colOid";
+      this.colOid.Visible = true;
+      this.colOid.VisibleIndex = 0;
+      // 
+      // colName
+      // 
+      this.colName.FieldName = "Name";
+      this.colName.Name = "colName";
+      this.colName.Visible = true;
+      this.colName.VisibleIndex = 1;
+      // 
+      // gridColumn1
+      // 
+      this.gridColumn1.FieldName = "InvoiceContact!";
+      this.gridColumn1.Name = "gridColumn1";
+      this.gridColumn1.Visible = true;
+      this.gridColumn1.VisibleIndex = 2;
+      // 
+      // gridColumn2
+      // 
+      this.gridColumn2.FieldName = "InvoiceContact!Key";
+      this.gridColumn2.Name = "gridColumn2";
+      this.gridColumn2.Visible = true;
+      this.gridColumn2.VisibleIndex = 3;
+      // 
+      // gridColumn3
+      // 
+      this.gridColumn3.FieldName = "InvoiceAddress!";
+      this.gridColumn3.Name = "gridColumn3";
+      this.gridColumn3.Visible = true;
+      this.gridColumn3.VisibleIndex = 4;
+      // 
+      // gridColumn4
+      // 
+      this.gridColumn4.FieldName = "InvoiceAddress!Key";
+      this.gridColumn4.Name = "gridColumn4";
+      this.gridColumn4.Visible = true;
+      this.gridColumn4.VisibleIndex = 5;
+      // 
+      // CustomerList
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.Controls.Add(this.gridControl1);
+      this.Controls.Add(this.ribbonControl1);
+      this.Name = "CustomerList";
+      this.Size = new System.Drawing.Size(816, 572);
+      this.Load += new System.EventHandler(this.CustomerList_Load);
+      ((System.ComponentModel.ISupportInitialize)(this.customerCollection)).EndInit();
+      ((System.ComponentModel.ISupportInitialize)(this.unitOfWork)).EndInit();
+      ((System.ComponentModel.ISupportInitialize)(this.ribbonControl1)).EndInit();
+      ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).EndInit();
+      ((System.ComponentModel.ISupportInitialize)(this.gridView1)).EndInit();
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+    private DevExpress.Xpo.UnitOfWork unitOfWork;
+    private DevExpress.Xpo.XPCollection customerCollection;
+    private DevExpress.XtraBars.BarButtonItem saveChangesItem;
+    private DevExpress.XtraBars.BarButtonItem discardChangesItem;
+    private DevExpress.XtraBars.BarButtonItem printExportItem;
+    private DevExpress.XtraBars.Ribbon.RibbonControl ribbonControl1;
+    private DevExpress.XtraBars.Ribbon.RibbonPage ribbonPage1;
+    private DevExpress.XtraBars.Ribbon.RibbonPageGroup ribbonPageGroup1;
+    private DevExpress.XtraBars.Ribbon.RibbonPageGroup ribbonPageGroup2;
+    private DevExpress.XtraBars.Ribbon.RibbonPageCategory ribbonPageCategory1;
+    private DevExpress.XtraGrid.GridControl gridControl1;
+    private DevExpress.XtraGrid.Views.Grid.GridView gridView1;
+    private DevExpress.XtraGrid.Columns.GridColumn colOid;
+    private DevExpress.XtraGrid.Columns.GridColumn colName;
+    private DevExpress.XtraGrid.Columns.GridColumn gridColumn1;
+    private DevExpress.XtraGrid.Columns.GridColumn gridColumn2;
+    private DevExpress.XtraGrid.Columns.GridColumn gridColumn3;
+    private DevExpress.XtraGrid.Columns.GridColumn gridColumn4;
+  }
+}
